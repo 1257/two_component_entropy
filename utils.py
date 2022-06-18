dadb346ea8a2afc97f988fbc78946483c527ee52
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-18 17:30:00 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -232,8 +232,8 @@ def twoComponentLoss(outputs, class_labels, superclass_labels):
     indices = torch.nonzero(mask) 
     
     for i in indices:
-      outputs = torch.cat((outputs[:,:i], outputs[:,i:]), axis = 1)
-      class_labels=torch.cat((class_labels[:,:i], class_labels[:,i:]), axis = 1)
+      outputs = torch.cat((outputs[:i,:], outputs[i:,:]), axis = 1)
+      class_labels=torch.cat((class_labels[:i], class_labels[i:]), axis = 1)
     
     #outs1=torch.tensor(outputs).cuda()
     #classes1=torch.tensor(class_labels).cuda()
