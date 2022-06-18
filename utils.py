0b9f2056aa02c241541d73a644079ca13f19595c
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-18 16:27:05 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -261,7 +261,7 @@ def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=Tru
     
     cifar100_trainset1_1, cifar100_trainset2 = torch.utils.data.random_split(cifar100_training, [settings.COMPLEX_TRAINSET_SIZE, 50000-settings.COMPLEX_TRAINSET_SIZE], generator=torch.Generator().manual_seed(0))
   
-   cifar100_trainset1_1=list(cifar100_trainset1_1)
+    cifar100_trainset1_1=list(cifar100_trainset1_1)
     for i in range(len(cifar100_trainset1_1)):
       cifar100_trainset1_1[i]=list(cifar100_trainset1_1[i])
       cifar100_trainset1_1[i].append(cifar100_trainset1_1[i][1])
