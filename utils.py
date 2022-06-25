d363f172d3e1635033394d9ed381ee3901951a2e
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-25 12:20:08 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -265,6 +265,8 @@ def twoComponentLoss(outputs, class_labels, superclass_labels):
     #with multiple outputs------------------------------------------------------------------------------------------
     gather_results = [[torch.gather(outputs[j], 0, torch.tensor(indexes[i]).cuda()) for i in range(20)] for j in range(len(class_labels))]
     print("\n\nsize of all gather:", len(gather_results))
+    max_results = [[torch.max(one_gather_result[j][i]) for i in range(20)] for j in range(len(class_labels))]
+    print("\n\nall maximums:", max_results)
     #------------------------------------------------------------------------------------------------------------
     
     #l1=loss(torch.tensor(coarse).cuda(), superclass_labels)
